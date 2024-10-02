d06a5baeb37537ec003763bbe71c7e973ec6b116
kiranpattar <pattar.kiran@gmail.com>
2024-10-02 14:55:37 +0530

    Add files via upload

---
+++ src/components/TaskForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <div id="formField">
+      <input
+        @keyup.enter="submitForm"
+        id="name"
+        placeholder="Name"
+        v-model.trim.lazy="formValues.name"
+        type="text"
+      />
+      <select v-model="formValues.priority">
+        <option value="critical">Critical</option>
+        <option value="moderate">Moderate</option>
+        <option value="low">Low</option>
+      </select>
+      <button>Add Task</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "TaskForm",
+  data() {
+    return {
+      formValues: {
+        name: "",
+        priority: "low",
+      },
+    };
+  },
+  methods: {
+    submitForm() {
+      this.$emit("submitTask", { ...this.formValues });
+      this.formValues.name = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+/*  */
+</style>
